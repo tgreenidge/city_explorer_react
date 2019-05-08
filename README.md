df7915add8c854671f0a78ee6ccccc6577e9a920
Tisha Greenidge <tisha.m.greenidge@gmail.com>
2019-05-08 10:33:21 -0700

    add city explorer scaffold

--- README.md
+++ README.md
@@ -1,68 +1,65 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Number and name of feature: App
 
-## Available Scripts
+Estimate of time needed to complete: 15 min
 
-In the project directory, you can run:
+Start time: 940
 
-### `npm start`
+Finish time: 946
 
-Runs the app in the development mode.<br>
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+Actual time needed to complete: 6 min
 
-The page will reload if you make edits.<br>
-You will also see any lint errors in the console.
 
-### `npm test`
+Number and name of feature: Header
 
-Launches the test runner in the interactive watch mode.<br>
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+Estimate of time needed to complete: 15min
 
-### `npm run build`
+Start time: 9:50
 
-Builds the app for production to the `build` folder.<br>
-It correctly bundles React in production mode and optimizes the build for the best performance.
+Finish time: 9:55
 
-The build is minified and the filenames include the hashes.<br>
-Your app is ready to be deployed!
+Actual time needed to complete: 5min
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
 
-### `npm run eject`
+Number and name of feature: Main
 
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
+Estimate of time needed to complete: 5 min
 
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+Start time: 10:00
 
-Instead, it will copy all the configuration files and the transitive dependencies (Webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
+Finish time: 10:05
 
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
+Actual time needed to complete: 5min
 
-## Learn More
 
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
+Number and name of feature: Search
 
-To learn React, check out the [React documentation](https://reactjs.org/).
+Estimate of time needed to complete: 15 min
 
-### Code Splitting
+Start time: 10:06
 
-This section has moved here: https://facebook.github.io/create-react-app/docs/code-splitting
+Finish time: 10:09
 
-### Analyzing the Bundle Size
+Actual time needed to complete: 3 min
 
-This section has moved here: https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size
 
-### Making a Progressive Web App
+Number and name of feature: Map
 
-This section has moved here: https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
+Estimate of time needed to complete: 5 min
 
-### Advanced Configuration
+Start time: 10:10
 
-This section has moved here: https://facebook.github.io/create-react-app/docs/advanced-configuration
+Finish time: 10:14
 
-### Deployment
+Actual time needed to complete: 4 min
 
-This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
 
-### `npm run build` fails to minify
 
-This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+Number and name of feature: Result
+
+Estimate of time needed to complete: 10min
+
+Start time: 10:15
+
+Finish time: 10:18
+
+Actual time needed to complete: 3 min
--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <h1>City Explorer</h1>
+        <p>Enter a location below to learn about the weather, events, restaurants, movies filmed there, and more!</p>
+      </div>
+    )
+  }
+}
+
+class Search extends Component {
+  render() {
+    return (
+      <div>
+        <input/>
+        <button>Search</button>
+      </div>
+    )   
+  }
+}
+
+class Map extends Component {
+  render() {
+    return (
+      <div className="map">
+      <img src="https://via.placeholder.com/300"/>
+      </div>
+    )
+  }
+}
+
+class Result extends Component {
+  render() {
+    return (
+      <div>
+        <section>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In ornare quam viverra orci. Sit amet tellus cras adipiscing enim eu. Viverra vitae congue eu consequat ac. Ornare arcu odio ut sem nulla. Placerat in egestas erat imperdiet sed. Egestas diam in arcu cursus euismod quis viverra nibh cras. Viverra vitae congue eu consequat. Fusce ut placerat orci nulla pellentesque dignissim enim sit. In ornare quam viverra orci sagittis eu volutpat odio facilisis. Vel quam elementum pulvinar etiam non. Consectetur adipiscing elit pellentesque habitant morbi tristique.</section>
+        <section>Tristique senectus et netus et malesuada fames ac. Vivamus at augue eget arcu. Justo donec enim diam vulputate. Praesent semper feugiat nibh sed pulvinar proin. Etiam dignissim diam quis enim lobortis scelerisque fermentum dui. Elit sed vulputate mi sit amet mauris commodo quis imperdiet. Sollicitudin nibh sit amet commodo nulla facilisi. Morbi tincidunt augue interdum velit euismod in pellentesque massa. Lacinia at quis risus sed vulputate odio ut enim. Integer enim neque volutpat ac tincidunt vitae. Feugiat nibh sed pulvinar proin gravida. Elementum sagittis vitae et leo duis. Adipiscing tristique risus nec feugiat. Arcu felis bibendum ut tristique et egestas. Massa id neque aliquam vestibulum morbi blandit cursus risus at. Tincidunt arcu non sodales neque sodales. Amet consectetur adipiscing elit duis tristique sollicitudin nibh sit. Erat nam at lectus urna duis. Augue interdum velit euismod in.</section>
+        <section>Blandit cursus risus at ultrices mi tempus. At erat pellentesque adipiscing commodo elit. Elit pellentesque habitant morbi tristique senectus et netus. Proin sagittis nisl rhoncus mattis rhoncus. Nunc sed id semper risus in hendrerit gravida rutrum. Auctor elit sed vulputate mi. Mauris pellentesque pulvinar pellentesque habitant morbi tristique senectus. Morbi non arcu risus quis varius quam quisque id diam. Pellentesque habitant morbi tristique senectus. Sagittis id consectetur purus ut faucibus pulvinar elementum integer enim. Ut ornare lectus sit amet est placerat in egestas erat. Risus pretium quam vulputate dignissim suspendisse in est ante in. Non enim praesent elementum facilisis leo. Odio tempor orci dapibus ultrices in iaculis. Gravida rutrum quisque non tellus orci ac.</section>
+        <section>Elementum nibh tellus molestie nunc non blandit massa enim. Ultricies mi quis hendrerit dolor magna eget. Vivamus arcu felis bibendum ut tristique et egestas quis. Elementum pulvinar etiam non quam lacus. Pulvinar etiam non quam lacus suspendisse faucibus interdum posuere lorem. Urna molestie at elementum eu facilisis. Amet aliquam id diam maecenas ultricies mi eget. Laoreet sit amet cursus sit amet dictum sit amet justo. Sed risus pretium quam vulputate dignissim suspendisse in est ante. Lectus proin nibh nisl condimentum id venenatis a condimentum. Urna neque viverra justo nec ultrices dui. Egestas purus viverra accumsan in nisl nisi scelerisque eu ultrices. Auctor urna nunc id cursus metus. Mi ipsum faucibus vitae aliquet nec ullamcorper sit.</section>
+        <section>Eget nunc lobortis mattis aliquam faucibus purus in massa tempor. Felis eget velit aliquet sagittis id consectetur. Bibendum enim facilisis gravida neque. Nunc aliquet bibendum enim facilisis gravida neque convallis a cras. Ut lectus arcu bibendum at varius vel pharetra vel. Integer feugiat scelerisque varius morbi. Sit amet facilisis magna etiam tempor. Id interdum velit laoreet id donec. Elementum pulvinar etiam non quam lacus suspendisse. Quam nulla porttitor massa id neque aliquam vestibulum morbi blandit. Metus dictum at tempor commodo ullamcorper a lacus. Lacus viverra vitae congue eu consequat ac felis. Risus feugiat in ante metus dictum at tempor commodo ullamcorper. Convallis convallis tellus id interdum velit. Elementum nibh tellus molestie nunc non blandit massa. Enim tortor at auctor urna nunc id cursus metus aliquam. Quam vulputate dignissim suspendisse in est ante. Odio facilisis mauris sit amet. At elementum eu facilisis sed odio morbi quis commodo.</section>
+      </div>
+    )
+  }
+}
+
+
+class Main extends Component {
+  render() {
+    return(
+      <React.Fragment>
+        <Search />
+        <Map />
+        <Result />
+      </React.Fragment>
+    )
+  }
+}
+
+class App extends Component {
+  render() {
+    return(
+      <React.Fragment>
+        <Header/>
+        <Main />
+      </React.Fragment> 
+    )
+  }
 }
 
 export default App;
