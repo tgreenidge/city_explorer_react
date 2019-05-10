e2e93b6ac78456f2844f4e42a097996a3bfddcc1
Tisha Greenidge <tisha.m.greenidge@gmail.com>
2019-05-10 15:55:07 -0700

    add classNmame to header

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Header extends Component {
   render() {
     return (
-      <div>
+      <div className='header'>
         <h1>City Explorer</h1>
         <p>Enter a location below to learn about the weather, events, restaurants, movies filmed there, and more!</p>
       </div>
